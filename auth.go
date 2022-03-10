1f278e5591bc30e297938f661c467933bd66ac5c
fghpdf <fghpdf@outlook.com>
2022-03-10 11:03:20 +0800

    fix: code

--- auth.go
+++ auth.go
@@ -95,7 +95,7 @@ type JWKKey struct {
 // AuthError auth error response
 type AuthError struct {
 	Message string `json:"message"`
-	Code    int    `json:code`
+	Code    int    `json:"code"`
 }
 
 // MiddlewareInit initialize jwt configs.
